b11a1d537fe1cfc8f60690a9fa91b5e809e9203f
Joe <joe@crimsonhexagon.us>
2018-02-24 18:09:57 -0500

    Added types for 64-bit processes

--- src/win32.rs
+++ src/win32.rs
@@ -5,8 +5,11 @@ use std::os::raw;
 
 pub type BOOL = i16;
 pub type BYTE = u8;
+pub type DWORD = u32;
+pub type DWORD64 = u64;
 pub type HANDLE = *mut raw::c_void;
 pub type LONG = i32;
+pub type LONGLONG = i64;
 pub type LPBYTE = *mut u8;
 pub type LPCONTEXT = *mut CONTEXT;
 pub type LPSECURITY_ATTRIBUTES = *mut raw::c_void;
@@ -18,9 +21,10 @@ pub type PBOOL = *mut BOOL;
 pub type PWOW64_CONTEXT = *mut WOW64_CONTEXT;
 pub type UCHAR = u8;
 pub type ULONG = u32;
+pub type ULONGLONG = u64;
 pub type ULONG_PTR = *mut u32;
-pub type DWORD = u32;
 pub type WORD = u16;
+pub type XMM_SAVE_AREA32 = XSAVE_FORMAT;
 
 pub const DEBUG_PROCESS: DWORD = 0x1;
 pub const CONTEXT_DEBUG_REGISTERS: DWORD =  0x00010010;
@@ -35,6 +39,78 @@ pub const THREAD_ALL_ACCESS: DWORD = 0x001F03FF;
 
 #[repr(C)]
 pub struct CONTEXT {
+    pub P1Home: DWORD64,
+    pub P2Home: DWORD64,
+    pub P3Home: DWORD64,
+    pub P4Home: DWORD64,
+    pub P5Home: DWORD64,
+    pub P6Home: DWORD64,
+    pub ContextFlags: DWORD,
+    pub MxCsr: DWORD,
+    pub SegCs: WORD,
+    pub SegDs: WORD,
+    pub SegEs: WORD,
+    pub SegFs: WORD,
+    pub SegGs: WORD,
+    pub SegSs: WORD,
+    pub EFlags: DWORD,
+    pub Dr0: DWORD64,
+    pub Dr1: DWORD64,
+    pub Dr2: DWORD64,
+    pub Dr3: DWORD64,
+    pub Dr6: DWORD64,
+    pub Dr7: DWORD64,
+    pub Rax: DWORD64,
+    pub Rcx: DWORD64,
+    pub Rdx: DWORD64,
+    pub Rbx: DWORD64,
+    pub Rsp: DWORD64,
+    pub Rbp: DWORD64,
+    pub Rsi: DWORD64,
+    pub Rdi: DWORD64,
+    pub R8: DWORD64,
+    pub R9: DWORD64,
+    pub R10: DWORD64,
+    pub R11: DWORD64,
+    pub R12: DWORD64,
+    pub R13: DWORD64,
+    pub R14: DWORD64,
+    pub R15: DWORD64,
+    pub Rip: DWORD64,
+    pub FltSave: XMM_SAVE_AREA32,
+    pub VectorRegister: [M128A; 26],
+    pub VectorControl: DWORD64,
+    pub DebugControl: DWORD64,
+    pub LastBranchToRip: DWORD64,
+    pub LastBranchFromRip: DWORD64,
+    pub LastExceptionToRip: DWORD64,
+    pub LastExceptionFromRip: DWORD64,
+}
+
+#[repr(C)]
+pub struct XSAVE_FORMAT {
+    pub ControlWord: WORD,
+    pub StatusWord: WORD,
+    pub TagWord: BYTE,
+    pub Reserved1: BYTE,
+    pub ErrorOpcode: WORD,
+    pub ErrorOffset: DWORD,
+    pub ErrorSelector: WORD,
+    pub Reserved2: WORD,
+    pub DataOffset: DWORD,
+    pub DataSelector: WORD,
+    pub Reserved3: WORD,
+    pub MxCsr: DWORD,
+    pub MxCsr_Mask: DWORD,
+    pub FloatRegisters: [M128A; 8],
+    pub XmmRegisters: [M128A; 16],
+    pub Reserved4: [BYTE; 96],
+}
+
+#[repr(C)]
+pub struct M128A {
+    pub Low: ULONGLONG,
+    pub High: LONGLONG,
 }
 
 #[repr(C)]
