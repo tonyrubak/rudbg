5233c35b21fb80eda34d289b9678bb0fef0d1dfd
Joe <joe@crimsonhexagon.us>
2018-02-23 23:13:20 -0500

    new testing for attaching the debugger

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,16 @@ extern crate kernel32;
 mod debugger;
 mod win32;
 
+use std::io::prelude::*;
+use std::io;
+
 fn main() {
     let mut debugger = debugger::Debugger::new();
-    debugger::load(&mut debugger, "c:\\windows\\system32\\calc.exe");
+    let mut input = String::new();
+    print!("Enter PID to which to attach: ");
+    io::stdout().flush();
+    let _ = io::stdin().read_line(&mut input);
+    let pid = str::parse::<u32>(&input[..].trim()).unwrap();
+    debugger::attach(&mut debugger, pid);
+    debugger::detach(debugger);
 }
